f100807a037aa08001961263e3f5b12cf803cba0
Geraldhzy <geraldhuang@163.com>
2019-05-26 22:49:28 +0800

    change loss

--- run_classifier.py
+++ run_classifier.py
@@ -640,14 +640,18 @@ def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
 
     logits = tf.matmul(output_layer, output_weights, transpose_b=True)
     logits = tf.nn.bias_add(logits, output_bias)
-    probabilities = tf.nn.softmax(logits, axis=-1)
-    log_probs = tf.nn.log_softmax(logits, axis=-1)
+    #sofmax loss
+    #probabilities = tf.nn.softmax(logits, axis=-1)
+    #log_probs = tf.nn.log_softmax(logits, axis=-1)
+    #one_hot_labels = tf.one_hot(labels, depth=num_labels, dtype=tf.float32)
+    #per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
+    #loss = tf.reduce_mean(per_example_loss)
 
-    one_hot_labels = tf.one_hot(labels, depth=num_labels, dtype=tf.float32)
-
-    per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
+    #contrastive loss
+    m = 0.3
+    probabilities = tf.nn.sigmoid(logits, axis=-1)
+    per_example_loss = 0.5*labels * (tf.square(1-probabilities)) + (1.0 - labels) * (tf.square(probabilities)) * tf.maximum(0.0, probabilities-m)
     loss = tf.reduce_mean(per_example_loss)
-
     return (loss, per_example_loss, logits, probabilities)
 
 
